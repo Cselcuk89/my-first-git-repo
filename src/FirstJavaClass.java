9ba7c287df049baa153a0f1dce90000a0150eca1
Cagri Selcuk <cagriselcuk89@gmail.com>
2022-03-26 14:55:12 -0500

    made some changes in first java class

---
+++ src/FirstJavaClass.java
@@ -0,0 +1,7 @@
+public class FirstJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Learning github");
+        System.out.println("this is another code");
+    }
+
+}
